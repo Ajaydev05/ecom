a9f93e4da249a6f4cbb1eca75fccdedec38773f0
Ajaydev05 <ajaydevtheni2003@gmail.com>
2026-02-13 07:28:04 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 export default function App() {
-  return <Home />;
+  return (
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
 }
